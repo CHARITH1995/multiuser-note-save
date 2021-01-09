dbdb1597a74263c6e2f70d2c6add65cccf861081
CHARITH1995 <prasanna.charith32@gmail.com>
2021-01-09 13:18:37 +0530

    db url change

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ const config = {
 
     filePath: '/files',
     TEST_URL : '/api/v1/textFile/',
-    DATA_BASE_URI : 'mongodb+srv://dbUser:dbUser@cluster0.w2z4e.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
 }
 
 module.exports = config;
